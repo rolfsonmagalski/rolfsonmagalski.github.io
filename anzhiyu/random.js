bc51365b1207b9c568fdf2e37034ef94fadb3543
kunkun <rolfsonmagalski851@gmail.com>
2025-06-03 13:52:48 +0800

    Site updated: 2025-06-03 13:52:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/01/测试帖子/"];function toRandomPost(){
+var posts=["2025/06/03/记录一下生活/","2025/06/01/测试帖子/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
